5e5f32600d9604a3039dd2d3a42b3dff6e50433a
Tom <twagner@cecaitra.org.ar>
2023-05-10 17:16:09 -0300

    prueba middleware

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from config.databse import engine, Base
 
 from middlewares.error_handler import ErrorHandler
+from middlewares.set_json_response import JSONMiddleware
 
 from routers.protocolo import protocolo_router
 from routers.ejecucion import ejecucion_router
@@ -13,6 +14,7 @@ app.title = 'Api para Desencriptador'
 app.version = '0.0.1'
 
 app.add_middleware(ErrorHandler)
+app.add_middleware(JSONMiddleware)
 app.include_router(protocolo_router)
 app.include_router(ejecucion_router)
 
